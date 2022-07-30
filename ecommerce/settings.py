88a2485a8c2084765b10aaf3e63b31dca50f48f4
ishtiak016 <ishtiak016@gmail.com>
2022-07-30 06:27:22 +0600

    2nd

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -123,7 +123,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')
 ]
